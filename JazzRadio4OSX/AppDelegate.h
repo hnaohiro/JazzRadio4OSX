6be5eff327cda9da050f78f755edef56dbbb45f9
Naohiro Hasegawa <q@hnaohiro.com>
2013-12-08 19:36:40 +0900

    Initial Commit

---
+++ JazzRadio4OSX/AppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.h
+//  JazzRadio4OSX
+//
+//  Created by naohiro on 2013/12/08.
+//  Copyright (c) 2013年 hnaohiro. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+- (IBAction)saveAction:(id)sender;
+
+@end
---
+++ JazzRadio4OSX/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  JazzRadio4OSX
+//
+//  Created by naohiro on 2013/12/08.
+//  Copyright (c) 2013年 hnaohiro. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
